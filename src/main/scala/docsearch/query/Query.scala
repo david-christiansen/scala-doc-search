7d46c29a0f792150c492e00688e5c8852f5a4bb8
David Christiansen <david@davidchristiansen.dk>
2010-11-25 22:32:06 +0100

    Start having query find matching members

--- src/main/scala/docsearch/query/Query.scala
+++ src/main/scala/docsearch/query/Query.scala
@@ -2,13 +2,24 @@ package docsearch.query
 
 import docsearch.types.MemType._
 
+import docsearch.types.Member
+
+import net.liftweb.mapper._
+
 case class Type(str: String)
 
 case class Query( path: Option[QPath], 
                   memType: Option[MemType],
                   name: Option[String],
                   args: List[List[QArg]],
-                  resultType: Type)
+                  resultType: Type) {
+  def findMatching(): List[Member] = {
+    val queryParams = 
+      (memType map (By(Member.memType, _)) toList) ++
+      (name map (By(Member.name, _)) toList) /* FIXME Add the rest! */
+    Member.findAll(queryParams:_*)
+  }
+}
                   
 case class QPath(components: List[String])
 
